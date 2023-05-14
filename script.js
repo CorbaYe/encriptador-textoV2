e3d90b2df9b2746c24386830606c526b2d73d12c
Yeison Cordoba <corba.ye99@gmail.com>
2023-05-13 23:29:16 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,119 @@
+	/*
+	*
+	*declaración de letiables
+	*input: caja de texto principal
+	*boton: encriptar
+	*label: muestra texto encriptado
+	*
+	*/
+	let input = document.getElementById("entradaTexto")
+	let botonEncriptar = document.getElementById("encriptar")
+	let botonDesencriptar = document.getElementById("desencriptar")
+	let ocultarLabel = document.getElementById("div-right")
+	let label = document.getElementById("nuevoTexto")
+	let botonCopiar = document.getElementById("copiar")
+	let ocultarBotonCopiar = document.getElementById("contenedor-copiar")
+	let informacion = document.getElementById("informacion")
+
+	let letrasEncriptadas = ["ai","enter","imes","ober","ufat"]
+	let letrasDesencriptadas = ['a','e','i','o','u']
+	let textArray
+
+	/*
+	*
+	*declaración de funciones
+	*encriptar
+	*desencriptar
+	*remplazarCaracter
+	*contarCaracteres
+	*mostrarOcultos
+	*copiarTexto
+	*
+	*/
+	  function contarCaracteres() {
+
+	    let contador = document.getElementById("contador");
+	    let maximo = input.getAttribute("maxlength");
+	    let actual = input.value.length;
+
+	    let restante = 0
+	    restante += actual;
+
+	    contador.innerHTML = restante;
+	}
+
+	function remplazarCaracter(caracter,posicion){
+
+		switch(caracter){
+			case 'a': textArray[posicion] = letrasEncriptadas[0]
+				break
+			case 'e': textArray[posicion] = letrasEncriptadas[1]
+				break
+			case 'i': textArray[posicion] = letrasEncriptadas[2]
+				break
+			case 'o': textArray[posicion] = letrasEncriptadas[3]
+				break
+			case 'u': textArray[posicion] = letrasEncriptadas[4]
+				break	
+		}
+	
+	}
+
+	function encriptar() {
+		
+		if (input.value.trim() != ""){
+			textArray = []
+			textArray = input.value.split("")
+			let caracter
+
+			for (let i = 0; i <= textArray.length; i++) {
+				caracter = textArray[i]
+				remplazarCaracter(caracter,i)
+			}
+
+			label.innerHTML = textArray.join('')
+			
+			input.value = "" 
+			contarCaracteres()
+			mostrarOcultos()
+			return false;
+		}
+
+	}
+
+	function desencriptar() {
+
+		if (input.value.trim() != "") {
+			label.innerHTML = input.value.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u")
+			mostrarOcultos()
+			input.value = "" 
+			contarCaracteres()		
+			return false;
+		}	
+	}
+
+	function copiarTexto(){
+
+	    let contenido = label.textContent;
+	    let temporal = document.createElement('input');
+
+	    temporal.value = contenido;
+	    document.body.appendChild(temporal);
+
+	    temporal.select();
+	    document.execCommand('copy');
+
+	    temporal.remove();
+	    alert("Texto copiado en el portapapeles")
+	}
+
+	function mostrarOcultos(){
+	    ocultarLabel.style.display = "block";
+	    ocultarBotonCopiar.style.display = "inline";
+	    informacion.style.display = "none"
+	}
+
+	botonEncriptar.onclick = encriptar
+	botonDesencriptar.onclick = desencriptar
+	botonCopiar.onclick = copiarTexto;
+
